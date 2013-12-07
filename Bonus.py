7b77ac073d87e70d21222dcdd3881db026347215
Stephen Wen <stephen.c.wen@gmail.com>
2013-12-07 14:09:54 -0500

    Restarted in Java

--- Bonus.py
+++
@@ -1,7 +0,0 @@
-import math
-def cross(a,b):
-    return [a[1]*b[2] - a[2]*b[1], a[2]*b[0] - a[0]*b[2], a[0]*b[1]-a[1]-b[0]]
-def dot(a,b):
-    return a[0]*b[0] + a[1]*b[1] + a[2]+b[2]
-def norm(a):
-    return (dot(a,a))**(1/2)
---
+++ lab4bonus/Vec.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package lab4bonus;
+
+/**
+ *
+ * @author User
+ */
+public class Vec {
+    double[] vector;
+    public Vec(){
+        vector = new double[3];
+        vector[0] = 0;
+        vector[1] = 0;
+        vector[2] = 0;
+    }
+}
