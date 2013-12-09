07b2750b061d74de6aa3214536f83057b05b9c01
Simon Huang <simon.yx.huang@gmail.com>
2013-12-09 16:05:43 -0500

    added rotation
    blah

--- lab4bonus/Coil.java
+++ lab4bonus/Coil.java
@@ -16,17 +16,16 @@ public class Coil {
     public double length;
     public double radius;
     public Vec position;
-    public Vec direction;
     public boolean cw;
     public Vec[] points;
+    public Vec[] wires;
 
-    public Coil (int n, double l, double r, Vec pos, Vec dir, boolean clock) {
+    public Coil (int n, double l, double r, Vec pos, boolean clock, double theta, double phi) {
 
         turns = n;
         length = l;
         radius = r;
         position = pos;
-        direction = dir;
         cw = clock;
   
         points = new Vec[16 * n ];
@@ -36,11 +35,30 @@ public class Coil {
             points[i] = new Vec(Math.cos(theta), Math.sin(theta), theta);
         }
 
-        rotate();
+        rotate(theta, phi);
+        
+        wires = new Vec[points.length - 1];
+        for (int i = 0; i < points.length - 1; i++) {
+            wires[i] = Vec.subtract(points[i+1], points[i]);
+        }
     }
 
-    private void rotate () {
-
+    private void rotate (double theta, double phi) {
+        // prefetch the existing vector
+        double x = position.getX();
+        double y = position.getY();
+        double z = position.getZ();
+        // apply x rotation matrix
+        position.setY(y*Math.cos(theta) - z*Math.sin(theta));
+        position.setZ(y*Math.sin(theta) + z*Math.cos(theta));
+        
+        // prefetch the new vector
+        x = position.getX();
+        y = position.getY();
+        z = position.getZ();
+        // apply y rotation matrix
+        position.setX(x*Math.cos(theta)+z*Math.sin(theta));
+        position.setZ(-x*Math.sin(theta)+z*Math.cos(theta));
     }
 
 }
